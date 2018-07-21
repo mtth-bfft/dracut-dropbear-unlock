02c2db95b6ea64cd8a52b09f56836f7302377587
Pouya <pouya@nohup.io>
2018-07-21 21:00:18 +0200

    remove no-longer supported dropbear option -m

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 SSHD_PORT=2222
 if ! [ -e /tmp/dropbear.pid ]; then
-	dropbear -s -j -k -m -p "$SSHD_PORT" -P /tmp/dropbear.pid -E >/tmp/dropbear.log 2>&1
+	dropbear -s -j -k -p "$SSHD_PORT" -P /tmp/dropbear.pid -E >/tmp/dropbear.log 2>&1
 	err=$?
 	if [ $err -ne 0 ]; then
 		warn "Failed to start SSH server: error code $err"
